6f5ecb28258b3847f2c72a0a0a3341cc52b42f1b
Swetha <swethaannadhurai@gmail.com>
2025-07-02 11:13:51 +0530

    cors updateddd

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://your-backend-api.onrender.com/api',
+  baseURL: 'https://mini-product-feedback-board.onrender.com',
   withCredentials: true,
 });
 
