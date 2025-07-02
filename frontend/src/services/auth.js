043c20fc21c7ee2b247d145b3fbd7e22fa68bdbf
Swetha <swethaannadhurai@gmail.com>
2025-07-02 12:47:41 +0530

    api update

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://mini-product-feedback-board.onrender.com',
+  baseURL: 'https://mini-product-feedback-board.onrender.com/api',
   withCredentials: true,
 });
 
 export const login = (data) => API.post('/auth/login', data);
 export const register = (data) => API.post('/auth/register', data);
 export const logout = () => API.post('/auth/logout');
+
