aa61790d9f76db2be0efc15de3da10f68153c455
Swetha <swethaannadhurai@gmail.com>
2025-07-02 13:14:56 +0530

    controller update

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,11 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user'
-  }
+  role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 
@@ -21,9 +17,10 @@ userSchema.pre('save', async function (next) {
 });
 
 
-userSchema.methods.matchPassword = function (enteredPassword) {
-  return bcrypt.compare(enteredPassword, this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
+
 
