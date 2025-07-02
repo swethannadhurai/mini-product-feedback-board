8f4792acb786bf56c6770ff4cfd8c430700e00e8
Swetha <swethaannadhurai@gmail.com>
2025-07-02 12:58:14 +0530

    auth changes

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://mini-product-feedback-board.onrender.com/api',
+  baseURL: 'https://mini-product-feedback-board.onrender.com',
   withCredentials: true,
 });
 
-export const login = (data) => API.post('/auth/login', data);
-export const register = (data) => API.post('/auth/register', data);
-export const logout = () => API.post('/auth/logout');
+export const login = (data) => API.post('api/auth/login', data);
+export const register = (data) => API.post('api/auth/register', data);
+export const logout = () => API.post('api/auth/logout');
 
